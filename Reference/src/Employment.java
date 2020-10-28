50c5b2283e62c9707ce2a1940046ec8f298ffafb
KamilAdd-Byte <kamillodzinski09@gmail.com>
2020-10-28 19:37:22 +0100

    Reference

--- Reference/src/Employment.java
+++ Reference/src/Employment.java
@@ -1,3 +1,7 @@
 public enum Employment {
-    FULL_TIME, PART_TIME, CONTRACT
+    FULL_TIME, PART_TIME, CONTRACT;
+
+    boolean checkEmployeeEmployment(Employee employee){
+        return this == employee.getEmployment();
+    }
 }
